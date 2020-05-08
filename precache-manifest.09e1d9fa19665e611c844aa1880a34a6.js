a77f686fac1a135dfe83fa2a15a457a56233055a
Oxa <oxakost@gmail.com>
2020-05-08 22:56:48 +0300

    Updates

---
+++ precache-manifest.09e1d9fa19665e611c844aa1880a34a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad87a451f95e06b1c7a43e81ac532e66",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "67669e7892368e024949",
+    "url": "/counter/static/css/main.0ce262e4.chunk.css"
+  },
+  {
+    "revision": "d7ef00f0b93d7c9684ff",
+    "url": "/counter/static/js/2.8860b37a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.8860b37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67669e7892368e024949",
+    "url": "/counter/static/js/main.c553adf6.chunk.js"
+  },
+  {
+    "revision": "77418c66201455f0b034",
+    "url": "/counter/static/js/runtime-main.7f528994.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counter/static/media/logo.5d5d9eef.svg"
+  }
+]);
